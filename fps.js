c7dd6b6795c4ca40f9a422ea91e752174b24d4d0
[nototiger] <[tai390_noto@yahoo.co.jp]>
2021-05-05 16:32:37 +0900

    視界が当たっているかの判定まで

--- fps.js
+++ fps.js
@@ -8,12 +8,11 @@ class Vec2 {
     this.y = y;
   }
   /**
-   *
    * @param {Vec2} b
    */
   add(b) {
     let a = this;
-    return new Vec2(a.x+b.x, a.y+b.y);
+    return new Vec2(a.x + b.x, a.y + b.y);
   }
   /**
    *
@@ -21,7 +20,13 @@ class Vec2 {
    */
   sub(b) {
     let a = this;
-    return new Vec2(a.x-b.x, a.y-b.y);
+    return new Vec2(a.x - b.x, a.y - b.y);
+  }
+  copy() {
+    return new Vec2(this.x, this.y);
+  }
+  mult(s) {
+    return new Vec2(s * this.x, s * this.y);
   }
 }
 class Ray2 {
@@ -40,7 +45,7 @@ class Ray2 {
    * @param {Vec2} end
    */
   static with2p(begin, end) {
-    return new Ray2(begin, end.sub(begin))
+    return new Ray2(begin, end.sub(begin));
   }
   // 線分の始点
   get begin() {
@@ -50,7 +55,32 @@ class Ray2 {
   get end() {
     return this.pos.add(this.way);
   }
-}class Player {
+  intersection(r2) {
+    let r1 = this;
+    if (abs(r1.way.x) < 0.01) r1.way.x = 0.01;
+    if (abs(r2.way.x) < 0.01) r2.way.x = 0.01;
+    let t1 = r1.way.y / r1.way.x;
+    let t2 = r2.way.y / r2.way.x;
+    let x1 = r1.pos.x;
+    let x2 = r2.pos.x;
+    let y1 = r1.pos.y;
+    let y2 = r2.pos.y;
+    let sx = (t1 * x1 - t2 * x2 - y1 + y2) / (t1 - t2);
+    let sy = t1 * (sx - x1) + y1;
+    if (
+      sx > min(r1.begin.x, r1.end.x) &&
+      sx < max(r1.begin.x, r1.end.x) &&
+      sx > min(r2.begin.x, r2.end.x) &&
+      sx < max(r2.begin.x, r2.end.x)
+    ) {
+      return new Vec2(sx, sy);
+    }
+    else {
+      return null;
+    }
+  }
+}
+class Player {
   constructor() {
     this.pos = new Vec2(0, 0);
     this.angle = 0;
@@ -70,20 +100,48 @@ function setup() {
 function draw() {
   background(60);
   strokeWeight(3);
-  stroke('white');
+  stroke("white");
   let walls = [
-    Ray2.with2p(new Vec2(50,50), new Vec2(100,300)),
-    Ray2.with2p(new Vec2(100,300), new Vec2(250,200)),
-    Ray2.with2p(new Vec2(250,200), new Vec2(50,50)),
-  ]
+    // Ray2.with2p(new Vec2(50,50), new Vec2(100,300)),
+    // Ray2.with2p(new Vec2(100,300), new Vec2(250,200)),
+    Ray2.with2p(new Vec2(250, 200), new Vec2(50, 50)),
+  ];
   for (let wall of walls) {
     line(wall.begin.x, wall.begin.y, wall.end.x, wall.end.y);
   }
 
   // プレイヤー描画
-  stroke('yellow');
+  stroke("yellow");
   strokeWeight(24);
   point(player.pos.x, player.pos.y);
+
+  // 視界の向き変更
+  if (keyIsDown(LEFT_ARROW)) player.angle -= PI / 60;
+  if (keyIsDown(RIGHT_ARROW)) player.angle += PI / 60;
+
+  // プレイヤーの視界描画
+  let fov = PI / 2;
+  let centerAngle = player.angle;
+  let leftAngle = player.angle - fov / 2;
+  let rightAngle = player.angle + fov / 2;
+  for (let angle = leftAngle; angle < rightAngle; angle += fov / 10) {
+    strokeWeight(2);
+    let pWay = new Ray2(
+      player.pos.copy(),
+      new Vec2(cos(angle), sin(angle)).mult(100) //三角関数の値域が１なので100倍
+    );
+    line(pWay.begin.x, pWay.begin.y, pWay.end.x, pWay.end.y);
+
+    // すべての壁との交点を求める
+    for (let wall of walls) {
+      let hitPos = pWay.intersection(wall);
+      if (hitPos) {
+        strokeWeight(10);
+        point(hitPos.x, hitPos.y);
+
+      }
+    }
+  }
 }
 
 function touchMoved(event) {
